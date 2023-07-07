87c924bdc3e7f300432af21f10b20089761aadb3
Partha <80641510+parthadeori@users.noreply.github.com>
2023-07-07 10:27:11 +0530

    Delete Lesson 1 directory

--- Lesson 1/practice.py
+++
@@ -1,4 +0,0 @@
-# Use the print() function to display "Python is Awesome" in the terminal.
-
-# Write your codes below this line 👇👇
-
--- Lesson 1/solution.py
+++
@@ -1,5 +0,0 @@
-# Use the print() function to display "Python is Awesome!" in the terminal.
-
-# Write your codes below this line 👇👇
-
-print("Python is Awesome!")
