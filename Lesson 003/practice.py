9bad0d64984f325ca063eeda0810a345e07abe42
Partha <80641510+parthadeori@users.noreply.github.com>
2023-07-08 08:06:54 +0530

    Add files via upload

---
+++ Lesson 003/practice.py
@@ -0,0 +1,15 @@
+# Step 1: Create and assign variables
+# 1. Create a variable `name` and assign your name as a string to it.
+
+
+# 2. Create a variable `age` and assign your age as an integer to it.
+
+
+# 3. Create a variable `height` and assign your height as a float to it.
+
+
+# 4. Create a variable `is_student` and assign a boolean value `True` or `False` to it, depending on whether you are a student or not.
+
+
+# Step 2: Print variable values and data types
+
---
+++ Lesson 003/solution.py
@@ -0,0 +1,11 @@
+# Step 1: Create and assign variables
+name = "John Doe"
+age = 25
+height = 1.75
+is_student = True
+
+# Step 2: Print variable values and data types
+print("Name:", name, "- Data Type:", type(name))
+print("Age:", age, "- Data Type:", type(age))
+print("Height:", height, "- Data Type:", type(height))
+print("Is Student:", is_student, "- Data Type:", type(is_student))
