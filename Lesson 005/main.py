a60d6698b0087e28b6162965d31b8413bdaa871b
Partha <80641510+parthadeori@users.noreply.github.com>
2023-07-08 08:07:23 +0530

    Add files via upload

---
+++ Lesson 005/main.py
@@ -0,0 +1,46 @@
+# Addition
+x = 5
+y = 3
+
+result = x + y
+print(result)  # Output: 8
+
+
+# Subtraction
+x = 5
+y = 3
+
+result = x - y
+print(result)  # Output: 2
+
+
+# Multiplication
+x = 5
+y = 3
+
+result = x * y
+print(result)  # Output: 15
+
+
+# Division
+x = 10
+y = 3
+
+result = x / y
+print(result)  # Output: 3.3333333333333335
+
+
+# Floor Division
+x = 10
+y = 3
+
+result = x // y
+print(result)  # Output: 3
+
+
+# Modulo
+x = 10
+y = 3
+
+result = x % y
+print(result)  # Output: 1
