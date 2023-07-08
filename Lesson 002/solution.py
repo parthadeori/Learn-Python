ab884e4de28a6a365244f1cdc12ad42371bce613
Partha <80641510+parthadeori@users.noreply.github.com>
2023-07-08 08:06:14 +0530

    Add files via upload

---
+++ Lesson 002/solution.py
@@ -0,0 +1,12 @@
+# Exercise: Getting User Information
+# Prompt the user for their name
+name = input("Please enter your name: ")
+
+# Prompt the user for their age
+age = input("Please enter your age: ")
+
+# Prompt the user for their favorite color
+color = input("Please enter your favorite color: ")
+
+# Print a personalized message
+print("Hello,", name, "! You are", age, "years old and your favorite color is", color + ".")
